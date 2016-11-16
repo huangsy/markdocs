36aa97757c037abdcc6b77515ceca3748a38c2c8
huangshiyu@youzan.com <huangshiyu@youzan.com>
2016-11-16 17:17:24 +0800

    bugfix

--- index.js
+++ index.js
@@ -8,22 +8,24 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
 var gulpJsdoc2md = require('gulp-jsdoc-to-markdown');
+var pkg = require('./package.json');
+var pwd = process.env.PWD;
 
+var srcPath = path.resolve(pwd, 'src', '**', '*.js');
 var template = path.resolve(__dirname, 'templates', 'readme.hbs');
 
 function build() {
-    gulp.src('./src/**/*.js')
+    gulp.src(srcPath)
         .pipe(gulpJsdoc2md({ template: fs.readFileSync(template, 'utf8') }))
         .on('error', function (err) {
             gutil.log(chalk.red('jsdoc2md failed'), err.message)
         })
         .pipe(concat('README.md'))
-        .pipe(gulp.dest('api'));
-    gutil.log(chalk.green('Build Success!'));
+        .pipe(gulp.dest('api'))
 }
 
 program
-    .version('1.0.0')
+    .version(pkg.version)
     .command('build')
     .action(build);
 
